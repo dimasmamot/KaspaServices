a97e3cecde51c323e704ee5a7e9aa8d2aa49230c
root <root@master.pens.ac.id>
2018-12-09 20:41:14 +0700

    vscode bug

--- app/static/template/build_snoqtt.sh
+++ app/static/template/build_snoqtt.sh
@@ -5,4 +5,4 @@ sudo docker build -t snoqtt-pcap:5.9 \
 --build-arg PROTECTED_SUBNET={protected_subnet} \
 --build-arg EXTERNAL_SUBNET={external_subnet} \
 --build-arg OINKCODE={oinkcode} \
-https://github.com/dimasmamot/snoqtt-alpine-pcap.git#master
+https://github.com/dimasmamot/snoqtt-alpine.git#master
